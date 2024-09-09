76ce55641940b49998548e4f3e4a466f33b9be41
dhara-maru <dharamaru406@gmail.com>
2024-09-09 17:03:24 +0530

    7. Count odd & even digits of a number

---
+++ countOddEven.java
@@ -0,0 +1,30 @@
+//7. find total odd and total even digits of given number
+
+import java.util.*;
+
+class countOddEven {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+
+        int oddcount = 0;
+        int evencount = 0;
+        while (n != 0) {
+            int digit = n % 10;
+
+            if (digit % 2 == 0) {
+                evencount++;
+            } else {
+                oddcount++;
+            }
+
+            n /= 10;
+        }
+
+        System.out.println("Even digits are : " + evencount);
+        System.out.println("Odd digits are : " + oddcount);
+    }
+}
