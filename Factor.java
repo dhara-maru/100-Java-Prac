4cff417f40e7255ccf3c08c55a42be385fcab105
dhara-maru <dharamaru406@gmail.com>
2024-09-09 16:04:38 +0530

    3. Factor of given Number

---
+++ Factor.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+class Factor {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number : ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= 100; i++) {
+            if (n % i == 0) {
+                System.out.println(i);
+
+            }
+        }
+    }
+}
